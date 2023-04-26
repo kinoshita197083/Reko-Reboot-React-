dfd1ce3347fbc7cf0eaf74ccf32527853996aec0
kinoshita197083 <83145802+kinoshita197083@users.noreply.github.com>
2023-04-26 11:29:54 +1000

    React Router redesigned - for future Auth purpose

--- src/App.js
+++ src/App.js
@@ -1,39 +1,24 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
-import Navbar from './Components/Navbar/Navbar';
-import NavItem from './Components/Navbar/NavItems/NavItems';
-import { navlinks } from './Components/Navbar/navlinks'
 import HomePage from './Pages/Home/Home';
 import AboutPage from './Pages/About/About';
 import AnalysePage from './Pages/Analyse/Analyse';
 import QueryPage from './Pages/Query/Query';
+import Layout from './Layout';
 
 function App() {
 
   return (
-    <div className="App">
-      <Router>
-        <Navbar>
-          {navlinks.map((link) => {
-            return (
-              <NavItem
-                icon={link.icon}
-                page={link.page}
-                type={link.type}
-                key={link.id}
-              />
-            )
-          })}
-        </Navbar>
-        <Routes>
-          <Route path='/' element={<HomePage />} />
-          <Route path='/about' element={<AboutPage />} />
-          <Route path='/analyse' element={<AnalysePage />} />
-          <Route path='/query' element={<QueryPage />} />
-        </Routes>
-      </Router>
-    </div>
+    <Routes>
+      <Route path='/' element={<Layout />}>
+        {/* Public Routes */}
+        <Route path='/' element={<HomePage />} />
+        <Route path='/about' element={<AboutPage />} />
+        <Route path='/analyse' element={<AnalysePage />} />
+        <Route path='/query' element={<QueryPage />} />
+      </Route>
+    </Routes>
   );
 }
 
